d16811e2696afd2b59dd1e97078aa999b9e0240b
Loa Diaz <loa.diaz@eemi.com>
2018-01-24 23:58:38 +0100

    loa

---
+++ js/fim.js
@@ -0,0 +1,31 @@
+var popUpAct = document.getElementsByClassName('pop-up');
+	console.log(popUpAct);
+	setTimeout(function() {
+		popUpAct[0].classList.add('pop-up-active');
+	}, 3000); 
+
+
+// var ongletTime = document.getElementsByClassName('onglet');
+// 	console.log(ongletTime);
+
+// (function(){
+// 	var ongletTime = document.querySelector('.onglet');
+// 	var time = 0;
+
+// 	var time_start = setInterval(function(){
+// 		ongletTime[0] = time + 1;
+// 	}, 1000);
+// });
+
+
+// setInterval(function(){
+// 	alert('hello')
+// }, 4000);
+
+
+
+
+
+
+
+          
