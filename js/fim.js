f6066369d7855d09d5cfd2aa689adaace11ca3d0
Loa Diaz <loa.diaz@eemi.com>
2018-01-25 11:16:29 +0100

    loa

--- js/fim.js
+++ js/fim.js
@@ -1,27 +1,43 @@
 var popUpAct = document.getElementsByClassName('pop-up');
-	console.log(popUpAct);
+	// console.log(popUpAct);
 	setTimeout(function() {
 		popUpAct[0].classList.add('pop-up-active');
 	}, 3000); 
 
 
+var ongletTime = document.getElementsByClassName('onglet');
+	console.log(ongletTime);
+
+var time_start = setInterval(function(){
+	var time = 0;
+	this.ongletTime[0] = time + 1; 
+	}, 1000);
+
+
 // var ongletTime = document.getElementsByClassName('onglet');
-// 	console.log(ongletTime);
+// var btn = document.querySelectorAll('#occupe');
 
-// (function(){
-// 	var ongletTime = document.querySelector('.onglet');
-// 	var time = 0;
+// var time_start;
+// var time = 0;
 
-// 	var time_start = setInterval(function(){
-// 		ongletTime[0] = time + 1;
-// 	}, 1000);
-// });
+//     time_start = setInterval(function () {
+//         popUpAct[1].time = time + 1;
 
+//         var minutes = Math.floor(time / 60);
+//         var seconds = time % 60;
 
-// setInterval(function(){
-// 	alert('hello')
-// }, 4000);
+//         if (minutes < 10) {
+//             minutes = '0' + minutes;
+//         }
 
+//         if (seconds < 10) {
+//             seconds = '0' + seconds;
+//         }
+
+//         var text = minutes + ':' + seconds;
+
+//         time.textContent = text;
+//     }, 1000);
 
 
 
